3ecd533ba68669b788070bf9c212cd6727ad1cea
prethega <prethegasakthisangar@gmail.com>
2024-07-30 20:32:13 +0530

    java

--- servers.js
+++ servers.js
@@ -1,1 +1,39 @@
 console.log("pret");
+const express =require("express");
+const app =express()
+const {MongoClient} = require("mongodb");
+
+const url ="mongodb+srv://prethega:<password>@cluster0.a4chafd.mongodb.net/?retryWrites=true&w=majority"
+
+const dbname = "test";
+let db =""
+
+async function connectDb(){
+ try{
+
+    const client = await MongoClient.connect(url);
+    console.log("connect to mangodb");
+    db = client.db(dbname);
+ }
+ catch(error){
+    console.log(error);
+
+ }
+
+}
+connectDb().then(()=>{
+    app.listen(3000,()=>{
+        console.log("server running");
+    })
+})
+app.get("/",async(req,res)=>{
+    const data ={name:"etho",username:"therla");
+    const  result = await db.collection("user_sample").insertion(data);
+    console.log("data inserted");
+    res.send("data inserted");
+
+        
+    }
+})
+
+})
